25fc010e390d28dd987c0cab4091d0050b4dac86
Sönke Nommensen <snommensen@gmail.com>
2013-11-03 18:26:43 +0100

    Extracted infrastructure server code into own module.

--- server.js
+++ server.js
@@ -1,34 +1,5 @@
-var util = require("util");
-var _ = require("underscore");
-
-/**
- * Configure a static web content server using ExpressJS (http://expressjs.com).
- * Static content is served from the directory '/www'.
- */
-var express = require("express");
-var app = express();
-app.configure(function () {
-    app.use(app.router);
-    app.use(express.static(__dirname + "/www"));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-var host = "0.0.0.0";
-
-/**
- * Create a web server instance using the Node.js 'http' module.
- */
-var http = require("http");
-var port = 1337;
-var server = http.createServer(app);
-
-/**
- * Attach a SockJS (https://github.com/sockjs) server to the server instance.
- */
-var sockjs = require('sockjs');
-var sockjsServer = sockjs.createServer();
-sockjsServer.installHandlers(server, {prefix: '/data'});
-server.listen(port, host);
-console.log(' [*] Listening on ' + host + ':' + port);
+var _ = require('underscore');
+var sockjsServer = require('./sockjs-server');
 
 /**
  * Holds all connected clients.
@@ -44,31 +15,29 @@ var connectedClients = [];
  */
 var updateClients;
 
-sockjsServer.on('connection', function (connection) {
-    connectedClients.push(connection);
+function update(client) {
+    console.log('emit to client %s', client.id);
 
-    updateClients = _.after(connectedClients.length, updateFinished);
-
-    connection.on('data', function (data) {
-        util.log("Received data: " + JSON.stringify(data));
-        _.each(connectedClients, update);
-    });
-});
-
-var update = function (client) {
-    util.log("emit to client " + client.id);
-
-    client.write(JSON.stringify({ message: "Ping"}));
+    client.write(JSON.stringify({message: 'Ping'}));
 
     updateClients();
-};
+}
 
-var updateFinished = function () {
-    util.log("done emitting data to all clients");
-};
+function updateFinished() {
+    console.log('done emitting data to all clients');
+}
 
+var config = {host: '0.0.0.0', port: 1337, prefix: '/data'};
 
+sockjsServer.start(config, function (sockjs) {
+    sockjs.on('connection', function (connection) {
+        connectedClients.push(connection);
 
+        updateClients = _.after(connectedClients.length, updateFinished);
 
-
-
+        connection.on('data', function (data) {
+            console.log('Received data: %s', JSON.stringify(data));
+            _.each(connectedClients, update);
+        });
+    });
+});
