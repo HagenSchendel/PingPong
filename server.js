e793f46a26b5c4465e7b581814bdbfc8a3514bc3
Sönke Nommensen <snommensen@gmail.com>
2013-11-03 12:16:39 +0100

    Removed dependency to 'async' module. Restructured code. Added documentation.

--- server.js
+++ server.js
@@ -1,56 +1,74 @@
 var util = require("util");
-var async = require("async");
 var _ = require("underscore");
-var http = require("http");
+
+/**
+ * Configure a static web content server using ExpressJS (http://expressjs.com).
+ * Static content is served from the directory '/www'.
+ */
 var express = require("express");
 var app = express();
-var host = "0.0.0.0";
-var port = 1337;
-var server = http.createServer(app);
-
-var sockjs = require('sockjs');
-var sockjsServer = sockjs.createServer();
-sockjsServer.installHandlers(server, {prefix: '/data'});
-console.log(' [*] Listening on ' + host + ':' + port);
-server.listen(port, host);
-
-var connectedClients = [];
-
-sockjsServer.on('connection', function (connection) {
-    connectedClients.push(connection);
-    connection.on('data', function (data) {
-        util.log("Received data: " + JSON.stringify(data));
-    });
-});
-
-function updateClients() {
-    var clientsToNotify = _.clone(connectedClients);
-    async.forEach(
-        clientsToNotify,
-        function (client, done) {
-            util.log("emit to client " + client.id);
-            try {
-                client.write("Ping");
-            } catch (err) {
-                done(err);
-            }
-            done(null);
-        },
-        function (err) {
-            if (typeof err !== "undefined" && err !== null) {
-                util.log(err);
-            } else {
-                util.log("done emitting data to all clients");
-            }
-        }
-    );
-}
-
 app.configure(function () {
     app.use(app.router);
     app.use(express.static(__dirname + "/www"));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+var host = "0.0.0.0";
+
+/**
+ * Create a web server instance using the Node.js 'http' module.
+ */
+var http = require("http");
+var port = 1337;
+var server = http.createServer(app);
+
+/**
+ * Attach a SockJS (https://github.com/sockjs) server to the server instance.
+ */
+var sockjs = require('sockjs');
+var sockjsServer = sockjs.createServer();
+sockjsServer.installHandlers(server, {prefix: '/data'});
+server.listen(port, host);
+console.log(' [*] Listening on ' + host + ':' + port);
+
+/**
+ * Holds all connected clients.
+ * @type {Array}
+ */
+var connectedClients = [];
+
+/**
+ * Wrapper function that is only evaluated after being
+ * called {@code connectedClients.length} times, i.e. in
+ * this case after all clients have been updated.
+ * @type {Function}
+ */
+var updateClients;
+
+sockjsServer.on('connection', function (connection) {
+    connectedClients.push(connection);
+
+    updateClients = _.after(connectedClients.length, updateFinished);
+
+    connection.on('data', function (data) {
+        util.log("Received data: " + JSON.stringify(data));
+        _.each(connectedClients, update);
+    });
+});
+
+var update = function (client) {
+    util.log("emit to client " + client.id);
+    try {
+        client.write(JSON.stringify({ message: "Ping"}));
+    } catch (err) {
+        util.log(err);
+    }
+    updateClients();
+};
+
+var updateFinished = function () {
+    util.log("done emitting data to all clients");
+};
+
 
 
 
